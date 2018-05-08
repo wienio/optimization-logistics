8c5588759893fc3d607325ca4b6e43171cb20a26
wienio <wienio96@gmail.com>
2018-05-08 13:15:40 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const config = require('./config')
+const CutMethod = require('./cutMethod')
+
+var method1 = new CutMethod('sss', 1, 2)
+
+console.log(method1)
---
+++ js/cutMethod.js
@@ -0,0 +1,8 @@
+module.exports = class CutMethod {
+    
+    constructor(cutAmountTypeA, cutAmountTypeB, garbage) {
+        this.cutAmountTypeA = cutAmountTypeA
+        this.cutAmountTypeB = cutAmountTypeB
+        this.garbage = garbage
+    }
+}
